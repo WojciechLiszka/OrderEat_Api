16bcff43c7e6c214d590a733dc3d490bc0cf6930
WojciechLiszka <wliszka99@gmail.com>
2023-03-28 16:07:33 +0200

    added RegisterUser_ForInvalidRegisterUserDto_BadRequest tests

--- FastFood.ApiTest/Controller/AccountControllerTests.cs
+++ FastFood.ApiTest/Controller/AccountControllerTests.cs
@@ -37,18 +37,43 @@ namespace FastFood.ApiTest.Controller
         public async Task RegisterUser_ForValidRegisterUserDto_ReturnsOk()
         {
             // arrange
-            var dto = new RegisterUserCommand()
+            var command = new RegisterUserCommand()
             {
                 Email = "test@test.com",
                 Password = "testpassword",
                 ConfirmPassword = "testpassword",
-                Name="John Doe"
+                Name = "John Doe"
             };
-            var httpContent = dto.ToJsonHttpContent();
+            var httpContent = command.ToJsonHttpContent();
             // act
             var response = await _client.PostAsync($"{_route}/register", httpContent);
             // assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Theory]
+        [InlineData("test@test.com", "testpassword", "notthesamepassword", "John Doe")]
+        [InlineData("test@test.com", null, null, "John Doe")]
+        [InlineData("test@test.com", "testpassword", "notthesamepassword", null)]
+        [InlineData(null, "testpassword", "notthesamepassword", "John Doe")]
+        [InlineData("", "testpassword", "notthesamepassword", "John Doe")]
+        [InlineData("test@test.com", "testpassword", "notthesamepassword", "")]
+        [InlineData("test@test.com", "", "", "John Doe")]
+        public async Task RegisterUser_ForInvalidRegisterUserDto_BadRequest(string email, string password, string confirmPassword, string name)
+        {
+            // arrange
+            var command = new RegisterUserCommand()
+            {
+                Email = email,
+                Password = password,
+                ConfirmPassword = confirmPassword,
+                Name = name
+            };
+            var httpContent = command.ToJsonHttpContent();
+            // act
+            var response = await _client.PostAsync($"{_route}/register", httpContent);
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
     }
 }
