5f45de825957f9827164074c1424077dd8ca9c69
WojciechLiszka <wliszka99@gmail.com>
2023-03-28 15:07:29 +0200

    added fake autorization and authentication for test

--- FastFood.Api/Program.cs
+++ FastFood.Api/Program.cs
@@ -40,3 +40,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program
+{ }
--- FastFood.ApiTest/Controller/AccountControllerTests.cs
+++ FastFood.ApiTest/Controller/AccountControllerTests.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FastFood.ApiTest.Authorization;
+using FastFood.Infrastructure.Persistance;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FastFood.ApiTest.Controller
 {
-    public class AccountControllerTests
+    public class AccountControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly HttpClient _client;
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public AccountControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContextOptions = services
+                            .SingleOrDefault(service => service.ServiceType == typeof(Microsoft.EntityFrameworkCore.DbContextOptions<FastFoodDbContext>));
+                        services.Remove(dbContextOptions);
+                        services.AddMvc(option => option.Filters.Add(new FakeUserFilter()));
+                        services
+                         .AddDbContext<FastFoodDbContext>(options => options.UseInMemoryDatabase("FastFoodDb"));
+                    });
+                });
+            _client = _factory.CreateClient();
+        }
     }
 }
