9776d830a1fd621648001a4d75cded44c9e4c889
WojciechLiszka <wliszka99@gmail.com>
2023-04-26 15:52:02 +0200

    added Create_ForValidId_ReturnsOk test

--- FastFood.ApiTest/Controller/OrderControllerTest.cs
+++ FastFood.ApiTest/Controller/OrderControllerTest.cs
@@ -1,12 +1,123 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using FastFood.Domain.Entities;
+using FastFood.Domain.Models;
+using FastFood.Infrastructure.Persistance;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FastFood.ApiTest.Controller
 {
-    internal class OrderControllerTest
+    public class OrderControllerTest : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly HttpClient _adminClient;
+        private readonly AuthenticationSettings _authenticationSettings;
+        private readonly IConfiguration _configuration;
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _ownerClient;
+        private readonly HttpClient _userClient;
+
+        public OrderControllerTest(WebApplicationFactory<Program> factory)
+        {
+            _configuration = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .Build();
+            var authenticationSettings = new AuthenticationSettings();
+
+            _configuration.GetSection("Authentication").Bind(authenticationSettings);
+            _authenticationSettings = authenticationSettings;
+            _factory = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        var dbContextOptions = services
+                            .SingleOrDefault(service => service.ServiceType == typeof(Microsoft.EntityFrameworkCore.DbContextOptions<FastFoodDbContext>));
+                        services.Remove(dbContextOptions);
+
+                        services
+                         .AddDbContext<FastFoodDbContext>(options => options.UseInMemoryDatabase("FastFoodDb")
+                         .EnableSensitiveDataLogging());
+                    });
+                });
+
+            _adminClient = _factory.CreateClient();
+            var adminToken = GenerateJwtToken("Admin", "1");
+            _adminClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", adminToken);
+            _ownerClient = _factory.CreateClient();
+            var ownerToken = GenerateJwtToken("Owner", "2");
+            _ownerClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ownerToken);
+            _userClient = _factory.CreateClient();
+            var userToken = GenerateJwtToken("User", "3");
+            _userClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userToken);
+        }
+
+        [Fact]
+        public async Task Create_ForValidId_ReturnsOk()
+        {
+            //arrange
+            var restaurant = new Restaurant()
+            {
+                Name = "Name",
+                Description = "TestDescription",
+                ContactDetails = new RestaurantContactDetails
+                {
+                    ContactNumber = "111111111",
+                    Email = "test@email.com",
+                    Country = "TestCountry",
+                    City = "TestCity",
+                    Street = "TestStreet",
+                    ApartmentNumber = "1/10"
+                }
+            };
+            await SeedRestaurant(restaurant);
+            //act
+
+            var response = await _userClient.PostAsync($"/api/restaurant/{restaurant.Id}/order", null);
+            //assert
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        private string GenerateJwtToken(string roleName, string userId)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId),
+                new Claim(ClaimTypes.Email,"test@email.com"),
+                new Claim(ClaimTypes.Name, "John Doe"),
+                new Claim(ClaimTypes.Role, roleName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authenticationSettings.JwtKey));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(_authenticationSettings.JwtExpireDays);
+
+            var token = new JwtSecurityToken(_authenticationSettings.JwtIssuer,
+                _authenticationSettings.JwtIssuer,
+                claims,
+                expires: expires,
+                signingCredentials: cred);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
+
+        private async Task SeedRestaurant(Restaurant restaurant)
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var _dbContext = scope.ServiceProvider.GetService<FastFoodDbContext>();
+
+            _dbContext.Add(restaurant);
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
